fe2eab2e5bee3db1410a22186d5fbfd3fa8946ef
Jorge Arce <65187763+jorgitorr@users.noreply.github.com>
2023-02-13 14:07:15 +0100

    Update select_III.sql
    añadidos ejercicios select III

--- Ejercicios SQL-DQL-20210113/select_III.sql
+++ Ejercicios SQL-DQL-20210113/select_III.sql
@@ -162,3 +162,232 @@ INSERT INTO REGISTRA(cif, dni, codigo, medio)VALUES(4,2,10,"Teléfono");
 INSERT INTO REGISTRA(cif, dni, codigo, medio)VALUES(4,1,10,"Tarjeta postal");
 INSERT INTO REGISTRA(cif, dni, codigo, medio)VALUES(4,1,10,"Internet");
 INSERT INTO REGISTRA(cif, dni, codigo, medio)VALUES(2,4,15,"Internet");
+
+/*
+1 Averigua el DNI de todos los clientes.*/
+SELECT DNI FROM CLIENTE;
+
+/*
+2 Consulta todos los datos de todos los programas.*/
+
+SELECT * FROM PROGRAMA;
+
+/*
+3 Obtén un listado con los nombres de todos los programas.*/
+
+SELECT nombre FROM PROGRAMA;
+
+
+/*
+4 Genera una lista con todos los comercios.*/
+
+SELECT Nombre FROM COMERCIO;
+
+
+/*
+5 Genera una lista de las ciudades con establecimientos donde se venden programas, sin que aparezcan valores duplicados (utiliza DISTINCT).*/
+
+SELECT DISTINCT ciudad FROM COMERCIO JOIN DISTRIBUYE ON COMERCIO.CIF=DISTRIBUYE.CIF;
+
+
+/*
+6 Obtén una lista con los nombres de programas, sin que aparezcan valores.
+duplicados (utiliza DISTINCT).*/
+
+SELECT DISTINCT Nombre FROM COMERCIO;
+
+/*
+7 Obtén el DNI más 4 de todos los clientes.*/
+
+SELECT DNI+4 AS SUMA FROM CLIENTE;
+
+/*
+8 Haz un listado con los códigos de los programas multiplicados por 7.*/
+
+SELECT codigo*7 FROM PROGRAMA;
+
+/*
+9 ¿Cuáles son los programas cuyo código es inferior o igual a 10?*/
+
+SELECT codigo FROM PROGRAMA WHERE codigo<=10;
+
+/*
+10 ¿Cuál es el programa cuyo código es 11?*/
+
+SELECT codigo FROM PROGRAMA WHERE codigo=11;
+/*
+11 ¿Qué fabricantes son de Estados Unidos?*/
+
+SELECT nombre FROM FABRICANTE WHERE Pais="Estados Unidos";
+
+/*
+12 ¿Cuáles son los fabricantes no españoles? Utilizar el operador IN.*/
+
+
+
+/*
+13 Obtén un listado con los códigos de las distintas versiones de Windows.*/
+
+
+/*
+14 ¿En qué ciudades comercializa programas El Corte Inglés?*/
+
+
+
+/*
+15 ¿Qué otros comercios hay, además de El Corte Inglés? Utilizar el operador
+IN.*/
+
+
+
+
+/*
+16 Genera una lista con los códigos de las distintas versiones de Windows y
+Access. Utilizar el operador IN.*/
+
+
+
+
+/*
+17 Obtén un listado que incluya los nombres de los clientes de edades
+comprendidas entre 10 y 25 y de los mayores de 50 años. Da una solución con
+BETWEEN y otra sin BETWEEN.*/
+
+
+
+
+/*
+18 Saca un listado con los comercios de Sevilla y Madrid. No se admiten
+valores duplicados.*/
+
+
+
+/*
+19 ¿Qué clientes terminan su nombre en la letra “o”?*/
+
+
+
+
+/*
+20 ¿Qué clientes terminan su nombre en la letra “o” y, además, son mayores de 30 años?*/
+
+
+/*
+21 Obtén un listado en el que aparezcan los programas cuya versión finalice por una letra i, o cuyo nombre comience por una A o por una W.*/
+
+
+
+/*22 Obtén un listado en el que aparezcan los programas cuya versión finalice por una letra i, o cuyo nombre comience por una A y termine por una S.*/
+
+
+/*23 Obtén un listado en el que aparezcan los programas cuya versión finalice por una letra i, y cuyo nombre no comience por una A.*/
+/*24 Obtén una lista de empresas por orden alfabético ascendente.*/
+
+
+/*
+25 Genera un listado de empresas por orden alfabético descendente.*/
+
+
+/*
+26 Obtén un listado de programas por orden de versión*/
+/*27 Genera un listado de los programas que desarrolla Oracle.*/
+/*28 ¿Qué comercios distribuyen Windows?*/
+/*29 Genera un listado de los programas y cantidades que se han distribuido a El Corte Inglés de Madrid.*/
+
+/*
+30 ¿Qué fabricante ha desarrollado Freddy Hardest?*/
+
+
+/*
+31 Selecciona el nombre de los programas que se registran por Internet.*/
+
+
+/*
+32 Selecciona el nombre de las personas que se registran por Internet.*/
+
+
+
+/*
+33 ¿Qué medios ha utilizado para registrarse Pepe Pérez?*/
+
+
+/*
+34 ¿Qué usuarios han optado por Internet como medio de registro?*/
+
+
+
+/*
+35 ¿Qué programas han recibido registros por tarjeta postal?*/
+
+
+/*
+36 ¿En qué localidades se han vendido productos que se han registrado por Internet?*/
+
+
+/*
+37 Obtén un listado de los nombres de las personas que se han registrado por Internet, junto al nombre de los programas para los que ha efectuado el registro.*/
+
+
+/*38 Genera un listado en el que aparezca cada cliente junto al programa que ha registrado, el medio con el que lo ha hecho y el comercio en el que lo ha adquirido.*/
+
+
+/*39 Genera un listado con las ciudades en las que se pueden obtener los productos de Oracle.*/
+
+
+/*40 Obtén el nombre de los usuarios que han registrado Access XP.*/
+
+
+/*41 Nombre de aquellos fabricantes cuyo país es el mismo que ʻOracleʼ.(Subconsulta).*/
+
+
+/*42 Nombre de aquellos clientes que tienen la misma edad que Pepe Pérez.(Subconsulta).*/
+
+
+/*43 Genera un listado con los comercios que tienen su sede en la misma ciudad que tiene el comercio ʻFNACʼ. (Subconsulta).*/
+
+
+/*44 Nombre de aquellos clientes que han registrado un producto de la misma forma que el cliente ʻPepe Pérezʼ. (Subconsulta).*/
+
+
+/*45 Obtener el número de programas que hay en la tabla programas.*/
+
+
+/*46 Calcula el número de clientes cuya edad es mayor de 40 años.*/
+/*47 Calcula el número de productos que ha vendido el establecimiento cuyo CIF es 1.*/
+
+
+/*48 Calcula la media de programas que se venden cuyo código es 7.*/
+/*49 Calcula la mínima cantidad de programas de código 7 que se ha vendido*/
+
+/*
+50 Calcula la máxima cantidad de programas de código 7 que se ha vendido.*/
+
+
+/*
+51 ¿En cuántos establecimientos se vende el programa cuyo código es 7?*/
+
+
+
+/*
+52 Calcular el número de registros que se han realizado por Internet.*/
+
+
+
+/*
+53 Obtener el número total de programas que se han vendido en ʻSevillaʼ.*/
+
+
+
+/*
+54 Calcular el número total de programas que han desarrollado los fabricantes cuyo país es ʻEstados Unidosʼ.*/
+
+
+
+/*
+55 Visualiza el nombre de todos los clientes en mayúscula. En el resultado de la consulta debe aparecer también la longitud de la cadena nombre.*/
+
+
+
+
+/*
+56 Con una consulta concatena los campos nombre y versión de la tabla PROGRAMA.*/
