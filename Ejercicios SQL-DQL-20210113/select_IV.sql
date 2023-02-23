1806869f9dd47237c00efc07b736da296cfc47ca
jorgitorr <jorgearceruiz97@gmail.com>
2023-02-23 23:30:57 +0100

    nuevo

--- Ejercicios SQL-DQL-20210113/select_IV.sql
+++ Ejercicios SQL-DQL-20210113/select_IV.sql
@@ -75,47 +75,152 @@ WHERE e2.apellido = "FERNANDEZ");
 
 /*4. Mostrar en pantalla el apellido, oficio y salario de los empleados del
 departamento de ʻFERNÁNDEZʼ que tengan su mismo salario.*/
-SELECT * 
-FROM EMPLE e, DEPART d
-WHERE d.dept_no IN();
 
+SELECT e.apellido, e.oficio, e.salario
+FROM EMPLE e, DEPART d
+WHERE e.salario = (SELECT e2.salario
+                   FROM EMPLE e2
+                  WHERE e2.apellido = "FERNANDEZ") and e.dept_no IN(SELECT e2.dept_no
+                                                                   FROM EMPLE e2) and e.apellido NOT IN("FERNANDEZ")
+GROUP BY e.apellido;
 
 
 /*5. Mostrar los datos de los empleados que tengan un salario mayor que ʻGILʼ y
-que pertenezcan al departamento número 10.
-6. Mostrar los apellidos, oficios y localizaciones de los departamentos de cada
-uno de los empleados.
-Ejercicios de SQL. Consultas más complejas (I). Pág. 27. Seleccionar el apellido, el oficio y la localidad de los departamentos donde
-trabajan los ANALISTAS.
-8. Seleccionar el apellido, el oficio y salario de los empleados que trabajan en
-Madrid.
-9. Seleccionar el apellido, salario y localidad donde trabajan de los empleados
-que tengan un salario entre 200000 y 300000.
-10. Mostrar el apellido, salario y nombre del departamento de los empleados
-que tengan el mismo oficio que ʻGILʼ.
-11. Mostrar el apellido, salario y nombre del departamento de los empleados
-que tengan el mismo oficio que ʻGILʼ y que no tengan comisión.
-12. Mostrar los datos de los empleados que trabajan en el departamento de
-contabilidad, ordenados por apellidos.
-13. Apellido de los empleados que trabajan en Sevilla y cuyo oficio sea analista
-o empleado.
-14. Calcula el salario medio de todos los empleados.
-15. ¿Cuál es el máximo salario de los empleados del departamento 10?
-16. Calcula el salario mínimo de los empleados del departamento 'VENTAS'.
-17. Calcula el promedio del salario de los empleados del departamento de
-'CONTABILIDAD'.
-18. Mostrar los datos de los empleados cuyo salario sea mayor que la media de
-todos los salarios.
-19. ¿Cuántos empleados hay en el departamento número 10?
-20. ¿Cuántos empleados hay en el departamento de 'VENTAS'?
-21. Calcula el número de empleados que hay que no tienen comisión.
-22. Seleccionar el apellido del empleado que tiene máximo salario.
-23. Mostrar los apellidos del empleado que tiene el salario más bajo.
-24. Mostrar los datos del empleado que tiene el salario más alto en el
-departamento de 'VENTAS'.
-25. A partir de la tabla EMPLE visualizar cuántos apellidos de los empleados
-empiezan por la letra ʻA'.
-26. Dada la tabla EMPLE, obtener el sueldo medio, el número de comisiones no
-Ejercicios de SQL. Consultas más complejas (I). Pág. 3nulas, el máximo sueldo y el sueldo mínimo de los empleados del
-departamento 30.
+que pertenezcan al departamento número 10.*/
+
+SELECT * 
+FROM EMPLE e
+WHERE e.salario>(SELECT e2.salario
+                FROM EMPLE e2
+                WHERE e2.apellido = "GIL")
+                and e.dept_no IN(SELECT e2.dept_no
+                                FROM EMPLE e2
+                                WHERE e2.dept_no = 10)
+                                GROUP BY e.apellido;
+
+/*6. Mostrar los apellidos, oficios y localizaciones de los departamentos de cada
+uno de los empleados.*/
+SELECT e.apellido, e.oficio, d.loc
+FROM EMPLE e, DEPART d
+WHERE e.dept_no = d.dept_no
+
+
+/*7. Seleccionar el apellido, el oficio y la localidad de los departamentos donde
+trabajan los ANALISTAS.*/
+SELECT e.apellido, e.oficio, d.dept_no, d.loc
+FROM depart d, emple e
+WHERE e.dept_no = d.dept_no and e.oficio = "ANALISTA";
+
+
+
+/*8. Seleccionar el apellido, el oficio y salario de los empleados que trabajan en
+Madrid.*/
+SELECT e.apellido, e.oficio, e.salario
+FROM emple e, depart d
+WHERE d.loc = "Madrid" and d.dept_no = e.dept_no;
+
+
+/*9. Seleccionar el apellido, salario y localidad donde trabajan de los empleados
+que tengan un salario entre 200000 y 300000.*/
+
+SELECT e.apellido, e.salario, d.loc
+FROM emple e, depart d
+WHERE e.dept_no = d.dept_no and e.salario BETWEEN 200000 AND 300000; 
+
+
+
+/*10. Mostrar el apellido, salario y nombre del departamento de los empleados
+que tengan el mismo oficio que ʻGILʼ.*/
+
+SELECT e.apellido, e.salario, d.dept_no
+FROM depart d, emple e 
+WHERE e.dept_no = d.dept_no and e.oficio IN(SELECT e2.oficio
+                                           FROM emple e2
+                                           WHERE e2.apellido IN('GIL') and e.apellido NOT IN('GIL'));
+
+
+
+/*11. Mostrar el apellido, salario y nombre del departamento de los empleados
+que tengan el mismo oficio que ʻGILʼ y que no tengan comisión.*/
+
+
+
+
+/*12. Mostrar los datos de los empleados que trabajan en el departamento de
+contabilidad, ordenados por apellidos.*/
+
+
+
+
+
+/*13. Apellido de los empleados que trabajan en Sevilla y cuyo oficio sea analista
+o empleado.*/
+
+
+
+
+
+/*14. Calcula el salario medio de todos los empleados.*/
+
+
+
+
+/*15. ¿Cuál es el máximo salario de los empleados del departamento 10?*/
+
+
+
+
+
+/*16. Calcula el salario mínimo de los empleados del departamento 'VENTAS'.*/
+
+
+
+
+/*17. Calcula el promedio del salario de los empleados del departamento de
+'CONTABILIDAD'.*/
+
+
+
+
+/*18. Mostrar los datos de los empleados cuyo salario sea mayor que la media de
+todos los salarios.*/
+
+
+
+
+/*19. ¿Cuántos empleados hay en el departamento número 10?*/
+
+
+
+
+/*20. ¿Cuántos empleados hay en el departamento de 'VENTAS'?*/
+
+
+
+/*21. Calcula el número de empleados que hay que no tienen comisión.*/
+
+
+
+/*22. Seleccionar el apellido del empleado que tiene máximo salario.*/
+
+
+
+/*23. Mostrar los apellidos del empleado que tiene el salario más bajo.*/
+
+
+
+
+/*24. Mostrar los datos del empleado que tiene el salario más alto en el
+departamento de 'VENTAS'.*/
+
+
+
+/*25. A partir de la tabla EMPLE visualizar cuántos apellidos de los empleados
+empiezan por la letra ʻA'.*/
+
+
+
+
+/*26. Dada la tabla EMPLE, obtener el sueldo medio, el número de comisiones no
+nulas, el máximo sueldo y el sueldo mínimo de los empleados del departamento 30.
 */
