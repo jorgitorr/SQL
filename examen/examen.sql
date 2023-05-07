d70ac1dff0fbf2e8afe9330e1274359eeda89522
jorgitorr <jorgearceruiz97@gmail.com>
2023-05-07 21:15:22 +0200

    terminado ex sql

--- examen/examen.sql
+++ examen/examen.sql
@@ -24,3 +24,20 @@ WHERE p.id IN(SELECT c.idPinche
 			GROUP BY c.idPinche)
 AND p.id = e.id AND p1.id = e.id
 GROUP BY p.id;
+
+4.
+SELECT c.idCliente, p.idPlato, COUNT(p.idPlato)
+FROM cliente c, pedido p, incluye i, plato p1 
+WHERE c.idCliente = p.idCliente AND i.idPedido = p.idPedido AND i.idPlato=p.idPlato
+GROUP BY p.idPlato, c.idCliente 
+ORDER BY c.idCliente DESC;
+
+5.
+SELECT pt1.idPlato
+FROM plato pt1 
+WHERE pt1.idPlato = (SELECT pt.idPlato
+					FROM plato pt, contiene c, ingrediente i
+					WHERE pt.idPlato = c.idPlato AND c.idIngrediente = i.idIngrediente
+					GROUP BY pt.idPlato
+					HAVING MAX(c.cantidad))
+GROUP BY pt1.idPlato;
